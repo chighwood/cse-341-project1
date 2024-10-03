327fe37524f6b158b5fc522688e0f7fcdcd951e1
chighwood <hig23003@byui.edu>
2024-10-02 19:22:07 -0500

    Added error handling and validation to the routes

--- controllers/users.js
+++ controllers/users.js
@@ -11,10 +11,14 @@ const getAll = async (req, res) => {
 
 const getSingle = async (req, res) => {
     const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('users').find({ _id: userId });
-    result.toArray().then((users) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(users[0]);
+        const result = await mongodb.getDatabase().db().collection('users').find({ _id: userId });
+        result.toArray().then((users) => {
+            if (users.length > 0) {
+                res.setHeader('Content-Type', 'application/json');
+                res.status(200).json(users[0]);
+            } else {
+                res.status(404).json({ message: 'User not found.' });
+            }
     });
 };
 
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const { body } = router('express-validator')
 
 const usersController = require('../controllers/users');
+
+const userValidation = [
+    body('firstName').isLength({ min: 1 }).withMessage('Required'),
+    body('lastName').isLength({ min: 1 }).withMessage('Required'),
+    body('email').isEmail().withMessage('Invalid email format'),
+    body('favoriteColor').isString().withMessage('Enter any color'),
+    body('birthday').optional().isISO8601().withMessage('Invalid date format'),
+];
+
+const userUpdateValidation = [
+    body('firstName').optional().isLength({ min: 1 }).withMessage('First name must be at least 1 character'),
+    body('lastName').optional().isLength({ min: 1 }).withMessage('Last name must be at least 1 character'),
+    body('email').optional().isEmail().withMessage('Invalid email format'),
+    body('favoriteColor').optional().isString().withMessage('Enter any color'),
+    body('birthday').optional().isISO8601().withMessage('Invalid date format'),
+];
 
 router.get('/', usersController.getAll);
 
 router.get('/:id', usersController.getSingle);
 
-router.post('/', usersController.createUser);
+router.post('/', userValidation, usersController.createUser);
 
-router.put('/:id', usersController.updateUser);
+router.put('/:id', userUpdateValidation, usersController.updateUser);
 
 router.delete('/:id', usersController.deleteUser);
 
